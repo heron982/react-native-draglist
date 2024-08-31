947786d6dfc31565fb12a022c3f6dbabd4ae436e
Felipe Horizonte <felipedevlp@gmail.com>
2024-08-31 06:52:33 -0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -424,6 +424,8 @@ function CellRendererComponent<T>(props: CellRendererProps<T>) {
       {...rest}
       style={[
         style,
+        focusedIndex !== undefined &&
+          focusedIndex === index && { elevation: 1, zIndex: 999 },
         isActive
           ? {
               elevation: 1,
